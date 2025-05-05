0a82979497e55035c1b1411a5e5b249af35750d1
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2025-05-05 14:44:03 +0100

    fixed missing product_attributes_extractor for ProductAttributesExtraction; test cases for boostrap extraction builder; bumped version;

--- sator_app/bootstrap/extraction.py
+++ sator_app/bootstrap/extraction.py
@@ -1,7 +1,7 @@
-
 from sator_app.bootstrap.base import BaseBuilder
 
 from sator_core.ports.driven.extraction.attributes.patch import PatchAttributesExtractorPort
+from sator_core.ports.driven.extraction.attributes.product import ProductAttributesExtractorPort
 from sator_core.ports.driven.extraction.attributes.vulnerability import VulnerabilityAttributesExtractorPort
 
 
@@ -13,15 +13,18 @@ from sator_core.use_cases.extraction.attributes import (
 class ExtractionBuilder(BaseBuilder):
     def __init__(
             self, patch_attrs_extractor: PatchAttributesExtractorPort,
+            product_attrs_extractor: ProductAttributesExtractorPort,
             vuln_attrs_extractor: VulnerabilityAttributesExtractorPort, **kwargs
     ):
         super().__init__(**kwargs)
 
         self.patch_attributes_extractor = patch_attrs_extractor
+        self.product_attributes_extractor = product_attrs_extractor
         self.vulnerability_attributes_extractor = vuln_attrs_extractor
 
     def create_product_attributes_extraction(self) -> ProductAttributesExtraction:
         return ProductAttributesExtraction(
+            extractor_port=self.product_attributes_extractor,
             storage_port=self.storage_port
         )
 
