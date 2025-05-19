90372157e81210685df1b18ab0ad6a30db1b256a
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2025-05-19 21:44:16 +0100

    updated ResolutionBuilder and implemented tests; bumped version;

--- sator_app/bootstrap/resolution.py
+++ sator_app/bootstrap/resolution.py
@@ -2,6 +2,7 @@ from typing import List
 
 from sator_app.bootstrap.base import BaseBuilder
 
+from sator_core.ports.driven.repositories.oss import OSSRepositoryPort
 from sator_core.ports.driven.repositories.product import ProductRepositoryPort
 from sator_core.ports.driven.repositories.vulnerability import VulnerabilityRepositoryPort
 
@@ -13,11 +14,13 @@ from sator_core.use_cases.resolution.references import (
 
 class ResolutionBuilder(BaseBuilder):
     def __init__(
-            self, vuln_repos: List[VulnerabilityRepositoryPort], prod_repos: List[ProductRepositoryPort], **kwargs
+            self, vuln_repos: List[VulnerabilityRepositoryPort], prod_repos: List[ProductRepositoryPort],
+            oss_repos: List[OSSRepositoryPort], **kwargs
     ):
         super().__init__(**kwargs)
         self.vulnerability_repositories = vuln_repos
         self.product_repositories = prod_repos
+        self.oss_repositories = oss_repos
 
     def create_vulnerability_metadata_resolution(self) -> VulnerabilityMetadataResolution:
         return VulnerabilityMetadataResolution(
@@ -45,6 +48,8 @@ class ResolutionBuilder(BaseBuilder):
 
     def create_patch_references_resolution(self) -> PatchReferencesResolution:
         return PatchReferencesResolution(
+            oss_repositories=self.oss_repositories,  # Empty list as default
             oss_gateway=self.oss_gateway,
             storage_port=self.storage_port
         )
+
